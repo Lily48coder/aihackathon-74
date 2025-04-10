461700f68fb6f3e2ef797b9a9e92ca057517c16e
Lily48coder <tuliplily11flora@gmail.com>
2025-04-10 13:33:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 import SplashScreen from "@/pages/SplashScreen";
 import Index from "@/pages/Index";
-import Welcome from "@/pages/Welcome";
+import Welcome from "@/pages/Welcome"; 
 import SignIn from "@/pages/SignIn";
 import SignUp from "@/pages/SignUp";
 import Dashboard from "@/pages/Dashboard";
