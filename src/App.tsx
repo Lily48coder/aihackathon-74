790dcf1d8aff8accd58632d2a7620c68075cc525
Lily48coder <tuliplily11flora@gmail.com>
2025-04-10 13:32:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BotpressChatbot from "./BotpressChatbot";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -66,6 +67,7 @@ function App() {
             </Routes>
           </Router>
           <Toaster />
+         <BotpressChatbot />
         </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
