a36029e003065c1930bb74dede4824da7f9b3f82
Lily48coder <tuliplily11flora@gmail.com>
2025-04-10 13:44:09 +0530

    Update BotpressChatbot.tsx

--- src/BotpressChatbot.tsx
+++ src/BotpressChatbot.tsx
@@ -9,22 +9,21 @@ const BotpressChatbot = () => {
       window.botpressWebChat.init({
         botId: "93667c20-5b2b-4478-a8d5-2a3e4582bc8f",
         clientId: "f1c60e8e-baaf-488a-a167-c1827bedeb91",
-        configuration: {
-          composerPlaceholder: "type here to analyse...",
-          botName: "DiagnoPredict",
-          color: "#3B82F6",
-          variant: "solid",
-          themeMode: "light",
-          fontFamily: "inter",
-          radius: 1,
-          allowFileUpload: true
-        }
+        composerPlaceholder: "type here to analyse...",
+        botName: "DiagnoPredict",
+        color: "#3B82F6",
+        variant: "solid",
+        themeMode: "light",
+        fontFamily: "inter",
+        radius: 1,
+        allowFileUpload: true,
       });
     };
     document.body.appendChild(script);
   }, []);
 
-  return null; // It's just an injected script, nothing to render
+  return null;
 };
 
 export default BotpressChatbot;
+
